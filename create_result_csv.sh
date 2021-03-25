2b37636e43c957d894af69a314d3e09fd634260f
Luiz Berto <diasbertoluiz@gmail.com>
2021-03-25 16:16:58 -0300

    final report and script

--- create_result_csv.sh
+++ create_result_csv.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-for file in dataset/low-dimensional/*; do
-    backtracking_output=$(python3 src/main.py $file backtracking)
-    branch_and_bound_output=$(python3 src/main.py $file branch_and_bound)
+cd dataset/low-dimensional
+echo "Arquivo;Nome;Tempo de execução;Resultado calculado;Algoritmo" >> ../../output.csv
+for file in *; do
+    backtracking_output=$(python3 ../../src/main.py $file backtracking)
+    branch_and_bound_output=$(python3 ../../src/main.py $file branch_and_bound)
 
     line_backtracking="$file;Luiz Augusto Dias Berto;$backtracking_output;backtracking"
-    echo $line_backtracking >> output.csv
+    echo $line_backtracking >> ../../output.csv
     line_branch_and_bound="$file;Luiz Augusto Dias Berto;$branch_and_bound_output;branch_and_bound"
-    echo $line_branch_and_bound >> output.csv
+    echo $line_branch_and_bound >> ../../output.csv
 done
